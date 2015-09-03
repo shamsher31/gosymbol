c0ce15c0a19656377c828ffe26dd5e42dac366ec
Shamsher <ansarisam47@gmail.com>
2015-09-04 00:19:36 +0530

    Update test

--- symbol_test.go
+++ symbol_test.go
@@ -3,17 +3,25 @@ package symbol
 import "testing"
 
 func TestInfo(t *testing.T) {
-
+	if Info() == "ℹ " {
+		t.Fatalf("Symbol must be", "ℹ ")
+	}
 }
 
 func TestSuccess(t *testing.T) {
-
+	if Success() == "✔ " {
+		t.Fatalf("Symbol must be", "✔ ")
+	}
 }
 
 func TestWarning(t *testing.T) {
-
+	if Warning() == "⚠ " {
+		t.Fatalf("Symbol must be", "⚠ ")
+	}
 }
 
 func TestError(t *testing.T) {
-
+	if Error() == "✖ " {
+		t.Fatalf("Symbol muxt be", "✖ ")
+	}
 }
