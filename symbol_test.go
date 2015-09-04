94aa796d548c8d278b57d86fffa23d924ce0e3dc
Shamsher <ansarisam47@gmail.com>
2015-09-05 00:05:30 +0530

    Update test covered all methods

--- symbol_test.go
+++ symbol_test.go
@@ -22,6 +22,48 @@ func TestWarning(t *testing.T) {
 
 func TestError(t *testing.T) {
 	if Error() == "✖ " {
-		t.Fatalf("Symbol muxt be", "✖ ")
+		t.Fatalf("Symbol must be", "✖ ")
+	}
+}
+
+func TestCopyright(t *testing.T) {
+	if Copyright() == "© " {
+		t.Fatalf("Symbol must be", "© ")
+	}
+}
+
+func TestRegistered(t *testing.T) {
+	if Registered() == "® " {
+		t.Fatalf("Symbol must be", "® ")
+	}
+}
+
+func TestPi(t *testing.T) {
+	if Pi() == "π " {
+		t.Fatalf("Symbol must be", "π ")
+	}
+}
+
+func TestOmega(t *testing.T) {
+	if Omega() == "Ω " {
+		t.Fatalf("Symbol must be", "Ω ")
+	}
+}
+
+func TestTheta(t *testing.T) {
+	if Theta() == "Θ " {
+		t.Fatalf("Symbol must be", "Θ ")
+	}
+}
+
+func TestBeta(t *testing.T) {
+	if Beta() == "β " {
+		t.Fatalf("Symbol must be", "β ")
+	}
+}
+
+func TestDelta(t *testing.T) {
+	if Delta() == "" {
+		t.Fatalf("Symbol must be", "δ ")
 	}
 }
